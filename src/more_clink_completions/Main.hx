d44b0bc93e818efa59921003f1fabd9ae4e61f91
sebthom <sebthom@users.noreply.github.com>
2023-06-16 14:41:45 +0200

    prevent scripts from loading if clink is too old

--- src/more_clink_completions/Main.hx
+++ src/more_clink_completions/Main.hx
@@ -6,11 +6,18 @@
  */
 package more_clink_completions;
 
+import clink.api.Clink;
 import more_clink_completions.util.Macros;
 
 class Main {
 
    static function main():Void {
+      final clinkVersion = Clink.versionMajor * 1000000 + Clink.versionMinor * 1000 + Clink.versionPatch;
+      if (clinkVersion < 1004024 /*1.004.024*/) {
+         Sys.println("WARNING: Clink version too old! more-clinks-completions requires clink 1.4.24 or higher.");
+         return;
+      }
+
       Macros.registerCommandCompletions();
 
       #if register_dummy_command
